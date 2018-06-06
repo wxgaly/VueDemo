87bdd4b7b0d83e51ae2240ecbb15236c979bb87e
wangxuguang <nova_wangxuguang@126.com>
2018-06-06 10:20:09 +0800

    增加一个vue cli项目

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// import Api from './api/index.js'
+// import Utils from './utils/index.js'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+// Vue.prototype.$utils = Utils
+// Vue.prototype.$api = Api
+// Vue.use(ElementUI)
