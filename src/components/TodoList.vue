467d2accc8dc060c108db1c7500d7047410212e0
wangxuguang <nova_wangxuguang@126.com>
2018-06-12 17:20:23 +0800

    增加 todo list功能

---
+++ src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="todo-list">
+    <p>{{ msg }}</p>
+    <input v-model="" placeholder="edit me">
+    <button @click="confirm"></button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TodoList",
+    data() {
+      return {
+        msg: "this is the todo list demo.",
+
+      }
+    },
+    methods() {
+      confirm: function () {
+
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
